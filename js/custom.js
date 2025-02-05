09009132b17ab92a34c5142c6896352276cb9e68
pratheesh-ui <pratheeshpayyanayil@gmail.com>
2025-02-05 12:57:04 +0530

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -9,6 +9,29 @@ $(window).scroll(function(){
 
    
 AOS.init({
-  duration: 1200,
+  duration: 800,
+    offset:50,
   once: true,
 })
+
+
+
+$(document).ready(function(){
+	
+function isInView(el) {
+  var box = el.getBoundingClientRect();
+  return box.top < window.innerHeight && box.bottom >= 0;
+}
+
+window.addEventListener("scroll", function() {
+  var footer = document.getElementById("footer");
+  var visible = isInView(footer);
+    if(visible) {
+      footer.classList.add("footer_active");
+    } else {
+      footer.classList.remove("footer_active");
+    }
+});	
+    
+ 
+});
